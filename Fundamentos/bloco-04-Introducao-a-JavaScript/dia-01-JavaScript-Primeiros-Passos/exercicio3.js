3323b370a8e5ced4a9b8f0836634e46356bbd356
Leonardo Gonçalves <leonardorc.geo@gmail.com>
2021-11-26 19:51:06 -0300

    Resoluçao do terceiro ex

---
+++ Fundamentos/bloco-04-Introducao-a-JavaScript/dia-01-JavaScript-Primeiros-Passos/exercicio3.js
@@ -0,0 +1,11 @@
+const a = 20;
+const b = 6;
+const c = 5;
+
+if (a > b && a > c){
+    console.log('A constante a é a maior dos 3 valores');
+} else if(b > a && b > c){
+    console.log("A constante 'b' é a maior dos 3 valores");
+} else {
+    console.log("A constante 'c' é a maior dos 3 valores")
+}
