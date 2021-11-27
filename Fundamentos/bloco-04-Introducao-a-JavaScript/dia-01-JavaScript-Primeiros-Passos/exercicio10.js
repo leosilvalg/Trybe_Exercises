d8e85f35d9dc1ad8a757c6e25c06509edd13fc27
Leonardo Gonçalves <leonardorc.geo@gmail.com>
2021-11-27 14:48:15 -0300

    Corrigindo o ex10

--- Fundamentos/bloco-04-Introducao-a-JavaScript/dia-01-JavaScript-Primeiros-Passos/exercicio10.js
+++ Fundamentos/bloco-04-Introducao-a-JavaScript/dia-01-JavaScript-Primeiros-Passos/exercicio10.js
@@ -1,6 +1,7 @@
 const valorCusto = 20;
 const valorVenda = 45;
-const valorCustoTotal = valorCusto * 1.2;
+const taxa = 0.2;
+const valorCustoTotal = valorCusto * taxa;
 const lucro = (valorVenda - valorCustoTotal) * 1000;
 
 if (valorCusto < 0 || valorVenda < 0){
