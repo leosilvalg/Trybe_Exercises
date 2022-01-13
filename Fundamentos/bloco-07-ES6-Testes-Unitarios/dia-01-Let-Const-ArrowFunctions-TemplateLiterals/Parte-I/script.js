243e2e74b067865be51308f84a32061d0ee7b387
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-12 23:52:24 -0300

    Enviando o segundo exercício

--- Fundamentos/bloco-07-ES6-Testes-Unitarios/dia-01-Let-Const-ArrowFunctions-TemplateLiterals/Parte-I/script.js
+++ Fundamentos/bloco-07-ES6-Testes-Unitarios/dia-01-Let-Const-ArrowFunctions-TemplateLiterals/Parte-I/script.js
@@ -2,7 +2,7 @@
 Modifique a estrutura da função para que ela seja uma arrow function.
 Modifique as concatenações para template literals */
 
-const testingScope = (escopo) => {
+/*const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
@@ -14,4 +14,15 @@ const testingScope = (escopo) => {
   // console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
 }
 
-testingScope(false);
+testingScope(false);*/
+
+let oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens[0] = 2;
+oddsAndEvens[1] = 3;
+oddsAndEvens[2] = 4;
+oddsAndEvens[3] = 7;
+oddsAndEvens[4] = 10;
+oddsAndEvens[5] = 13;
+
+console.log(`${oddsAndEvens} Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
