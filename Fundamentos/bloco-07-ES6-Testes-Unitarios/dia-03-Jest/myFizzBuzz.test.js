b9fa6272a1b754d5861c940a04a0a99405d37d86
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-17 14:04:11 -0300

    Começando o terceiro ex

---
+++ Fundamentos/bloco-07-ES6-Testes-Unitarios/dia-03-Jest/myFizzBuzz.test.js
@@ -0,0 +1,8 @@
+/* A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número
+1 - Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
+2 - Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
+3 - Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
+4 - Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
+5 - Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado */
+
+const myFizzBuzz = require('./myFizzBuzz');
