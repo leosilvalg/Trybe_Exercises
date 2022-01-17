f52baaa269b86cf70b95aaa9e7b192b164eea462
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-17 12:50:50 -0300

    Resolvendo o segundo ex

--- Fundamentos/bloco-07-ES6-Testes-Unitarios/dia-03-Jest/myRemove.test.js
+++ Fundamentos/bloco-07-ES6-Testes-Unitarios/dia-03-Jest/myRemove.test.js
@@ -8,3 +8,11 @@ const remove =  require('./myRemove');
 test('Vereifica se é retornado o array sem o item', () => {
     expect(remove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
 });
+
+test('Vereifica se é retornado o array sem o item', () => {
+    expect(remove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+});
+
+test('Vereifica se é retornado o array sem o item', () => {
+    expect(remove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+});
