39292c68f1d569e7513a3ac525ade5a8c01fc8c7
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-17 12:41:14 -0300

    Resolvendo o primeiro item do ex 2

---
+++ Fundamentos/bloco-07-ES6-Testes-Unitarios/dia-03-Jest/myRemove.test.js
@@ -0,0 +1,10 @@
+/*A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado*/
+
+const remove =  require('./myRemove');
+
+test('Vereifica se é retornado o array sem o item', () => {
+    expect(remove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+});
