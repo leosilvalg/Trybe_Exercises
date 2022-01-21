37fe41be3dca09bbe19f1c5548508750265fde6d
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-21 17:40:48 -0300

    Realizando o ex 6

---
+++ Fundamentos/bloco-08-Higher-Order-Functions/dia-04-reduce/script6.js
@@ -0,0 +1,10 @@
+// 6. - Agora vamos criar um novo array de objetos a partir das informações abaixo, onde cada objeto terá o formato { name: nome do aluno, average: media das notas } . Para isso vamos assumir que a posição 0 de notas refere-se ao aluno na posição 0 de alunos , aqui além de reduce será necessário utilizar também a função map . Dica: Você pode acessar o index do array dentro de map , e você pode ver o objeto esperado na constante expected .
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const notes = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const objeto = students.map((student, item) => ({
+  name: student,
+  nota: (notes[item].reduce((acc, curr) => acc + curr, 0 / notes[item].length))
+}));
+
+console.log(objeto);
