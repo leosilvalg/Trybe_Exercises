089b25466c1c2b8eca9e302e6e5480b2efaf773d
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-21 19:09:38 -0300

    algumas alterações

--- Fundamentos/bloco-08-Higher-Order-Functions/dia-04-reduce/script1.js
+++ Fundamentos/bloco-08-Higher-Order-Functions/dia-04-reduce/script1.js
@@ -5,6 +5,6 @@ const arrays = [
   [4, 5, 6],
 ];
 
-const arrayFinal = arrays.reduce((acc, val) => acc.concat(val), []);
+const arrayFinal = arrays.reduce((acc, val) => acc.concat(val)); // O val vai ser o array da linha 3 na primeira iteração, depois o da 4 e depois o da 5.
 
 console.log(arrayFinal);
