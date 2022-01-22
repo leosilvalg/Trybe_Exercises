f7d1c941d57520222659e931d2ccc3b2cbb2f1c2
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-22 11:25:33 -0300

    Arrumando a posição dos exercícios

---
+++ Fundamentos/bloco-08-Higher-Order-Functions/dia-05-Spread-Rest-Destructuring/paraFixar/fixação2.js
@@ -0,0 +1,30 @@
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+// 1 - Temos dois objetos, um com informações pessoais de uma pessoa usuária e outro com informações referentes ao cargo desta pessoa usuária na empresa trappistEnterprise . Precisamos criar um terceiro objeto, que terá os dados pessoais e os dados de cargo juntos. Para isso, utilize o spread operator .
+
+// const trappist = {...user, ...jobInfos};
+
+// console.log(trappist);
+
+// 2 - Com o objeto em mãos, imprima no console uma frase utilizando os dados do objeto criado anteriormente. Para isso, utilize a desestruturação de objetos em conjunto com template literals .
+// Exemplo "Hi, my name is Maria, I'm 21 years old and I'm Brazilian. I work as a Software engineer and my squad is RLL-Rocket Landing Logic"
+
+const trappist = {...user, ...jobInfos};
+
+const frase = ({name, age, nationality, profession, squad, squadInitials}) => {
+
+  console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}.`);
+};
+
+frase(trappist);
+
