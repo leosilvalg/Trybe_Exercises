1dee134364a3aea0b7bf077d419f80d351771210
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-22 11:15:16 -0300

    Exercício de fixação sobre shorthand

---
+++ Fundamentos/bloco-08-Higher-Order-Functions/dia-04-reduce/fixação5.js
@@ -0,0 +1,10 @@
+// Agora é hora de praticar: altere a função getPosition utilizando a property shorthand .
+
+const getPosition = (latitude, longitude) => {
+  return {
+  latitude,
+  longitude,
+  };
+};
+
+console.log(getPosition(-19.8157, -43.9542));
