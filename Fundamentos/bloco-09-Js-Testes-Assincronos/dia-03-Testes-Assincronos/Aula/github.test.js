988ada286608ccd6dedf13686d6bf102d1d44fb3
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-02-01 17:32:20 -0300

    Primeiro teste com async/await

--- Fundamentos/bloco-09-Js-Testes-Assincronos/dia-03-Testes-Assincronos/Aula/github.test.js
+++ Fundamentos/bloco-09-Js-Testes-Assincronos/dia-03-Testes-Assincronos/Aula/github.test.js
@@ -2,15 +2,17 @@ const github = require('./github');
 
 const { getGitHubUserInfos } = github;
 
-decribe('quando chamada, a função getGitHubUserInfos', () => {
-  it('deve retornar um objeto contendo  name, company, twitter, bio e location', () => {
-    expect(getGitHubUserInfos('alexandremhm')).toEqual({
+describe('quando chamada, a função getGitHubUserInfos', () => {
+  it('deve retornar um objeto contendo  name, company, twitter, bio e location', async () => { //Lembrar de passar o async/await
+    const repositoryInfo = await getGitHubUserInfos('alexandremhm');
+    const expectedResult = {
       name: 'Matheus Alexandre',
       company: '@betrybe',
       twitter: undefined,
       bio: 'Web Development Student and \r\n' +
       'Summer Student at Trybe. Front-end: React, HTML, JS e CSS. #openToWork',
       location: 'Brazil'
-    });
+    }
+    expect(repositoryInfo).toEqual(expectedResult);
   });
 })
