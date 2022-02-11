665e772668fce031e000dcf6bbdd2f6bdbf0fee1
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-02-11 15:40:53 -0300

    Alguns ajustes nos estilos

--- Front-end/bloco-10-React-Introducao/dia-02-Componentes-React/my-pokedex/src/App.js
+++ Front-end/bloco-10-React-Introducao/dia-02-Componentes-React/my-pokedex/src/App.js
@@ -5,7 +5,7 @@ import Pokedex from './pokedex.jsx';
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen sm:bg-cover md:bg-cover lg:bg-cover xl:bg-contain bg-[url('https://i.pinimg.com/originals/d3/88/57/d38857eeb3ff01be07c05fbfa80d3385.png')]">
+    <div className=" items-center w-screen h-screen sm:bg-cover md:bg-cover lg:bg-cover xl:bg-contain bg-[url('https://i.pinimg.com/originals/d3/88/57/d38857eeb3ff01be07c05fbfa80d3385.png')]">
       <Header />
       <Pokedex />
     </div>
