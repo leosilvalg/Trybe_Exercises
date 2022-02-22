a18ede8afdd723f6500b9e4a5b8f1fd77da62718
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-02-22 15:36:34 -0300

    Adicionando eventos

--- Front-end/bloco-10-React-Introducao/dia-02-Componentes-React/my-pokedex/src/pokedex.jsx
+++ Front-end/bloco-10-React-Introducao/dia-02-Componentes-React/my-pokedex/src/pokedex.jsx
@@ -2,12 +2,40 @@ import React from "react";
 import Pokemon from './pokemon';
 import pokemons from "./data";
 
+
 class Pokedex extends React.Component {
+  constructor() {
+    super();
+
+
+    this.state = {
+      name: '',
+      pokemons,
+    };
+
+    this.filterByname = this.filterByname.bind(this);
+
+  }
+
+  filterByname(event) {
+    this.setState(
+      { name: event.target.value },
+      () => this.setState((prevState) => ({
+        pokemons: pokemons.filter((p) => p.name.toLowerCase().includes(prevState.name))
+      })),
+    );
+  }
+
   render() {
+    const { name, pokemons } = this.state 
+
+    console.log(name);
+
     return (
-      <section className="flex flex-wrap mx-10 my-10 justify-center items-center">
+      <div className="pokedex flex flex-wrap mx-10 my-10 justify-center items-center">
+        <input type="text" placeholder="Digite o nome do pokemon" value={ name } onInput={ this.filterByname } />
         {pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon}/>)}
-      </section>
+      </div>
     );
   }
 }
